a4f805cf558d363c68a7c05bade72c783292b493
8ball030 <8baller@station.codes>
2025-02-19 13:34:04 +0000

    feat:added-scripts

---
+++ scripts/generate_protocols.sh
@@ -0,0 +1,41 @@
+# Simple script to generate all the protocol files.
+set -euo pipefail
+
+SPEC_PATH='../specs/protocols/'
+
+tmp_agent_name='tmp_agent'
+function generate_protocol {
+    echo "Generating protocol $1"
+    rm -rf packages/eightballer/protocols/$1
+    aea create $tmp_agent_name
+    cd $tmp_agent_name
+    adev scaffold protocol $(echo $SPEC_PATH/$1.yaml)
+    aea publish --local --push-missing
+    cd ..
+    rm -rf $tmp_agent_name
+    rm -rf packages/author/agents/$tmp_agent_name
+    adev -v fmt -p  packages/eightballer/protocols/$1
+    adev -v lint -p packages/eightballer/protocols/$1
+    pytest packages/eightballer/protocols/$1
+}
+
+
+
+
+generate_protocol 'default'
+generate_protocol 'http'
+# generate_protocol 'pubsub'
+# generate_protocol 'websockets'
+# generate_protocol 'prometheus'
+# generate_protocol 'liquidity_provision'
+# generate_protocol 'default'
+# generate_protocol 'spot_asset'
+# generate_protocol 'ohlcv'
+# 
+# generate_protocol 'order_book'
+# generate_protocol 'markets'
+# 
+# generate_protocol 'orders'
+# generate_protocol 'positions'
+# 
+# generate_protocol 'orders'
